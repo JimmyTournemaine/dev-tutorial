8ae26f476e248cdebf7d98d90b88e7d95e843fc7
Jimmy Tournemaine <jimdeplouz@gmail.com>
2021-02-01 15:04:52 +0100

    Fix prod environment

--- dev-tutorial-app/src/environments/environment.prod.ts
+++ dev-tutorial-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiHost: 'http://localhost:3000',
   apiEndpoint: 'http://localhost:3000/api',
   socketEndpoint: 'http://localhost:3000',
   disableTerminal: false,
