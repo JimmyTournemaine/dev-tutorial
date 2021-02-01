dd0dee6f8f06e7567d3e0ff479ff556573951154
Jimmy Tournemaine <jimdeplouz@gmail.com>
2021-02-01 14:32:43 +0100

    Fix defaults

--- dev-tutorial-app/src/environments/environment.ts
+++ dev-tutorial-app/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiHost: 'http://localhost:3000',
   apiEndpoint: 'http://localhost:3000/api',
   socketEndpoint: 'http://localhost:3000',
-  disableTerminal: true,
+  disableTerminal: false,
 };
 
 /*
