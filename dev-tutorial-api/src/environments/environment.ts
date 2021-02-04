1e0e885d09cf55acec40442a61d2dc40468e4a74
Jimmy Tournemaine <jimdeplouz@gmail.com>
2021-02-04 22:24:35 +0100

    Cleaning

--- dev-tutorial-api/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-/* Ansible managed */
-export const environment = {
-  production: false,
-  docker: {
-    host: 'host.docker.internal',
-    port: 2375,
-  },
-  mongodb: 'mongodb://dev-tutorial-mongo-test:27017/dev_tutorial_api',
-};
